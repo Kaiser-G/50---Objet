cc527906cf1a44b8fb85610ff5eed700ed81b79f
Kaiser-G <kaiserg6969@gmail.com>
2024-02-26 14:24:00 +0100

    Exo 6 création du tableau

--- Livre.js
+++ Livre.js
@@ -60,4 +60,57 @@ class livre {
     alert("Le thème est inconnu");
     throw new Error("Erreur le thème n'est pas reconnu");
   }
+
+  constructor(
+    code = "NC",
+    titre = "NC",
+    auteur = "NC",
+    page = "NC",
+    theme = "NC"
+  ) {
+    this.Code = code;
+    this.Titre = titre;
+    this.Auteur = auteur;
+    this.Page = page;
+    this.Theme = theme;
+  }
+
+  Afficher = function () {
+    return (
+      "Code ISBN : " +
+      this.Code +
+      "Titre : " +
+      this.Titre +
+      "Auteur : " +
+      this.Auteur +
+      "Nombre de pages : " +
+      this.Page +
+      "Le thème : " +
+      this.Theme
+    );
+  };
+}
+
+//fonction qui récupère la saisi
+function recup_saisi() {
+  let resultat = new livre(
+    document.getElementById("code").value,
+    document.getElementById("titre").value,
+    document.getElementById("auteur").value,
+    document.getElementById("Nbp").value,
+    document.getElementById("theme").value
+  );
+  alert("Récap de saisi : " + resultat.Afficher());
+  return resultat;
+}
+
+//création du tableau
+function afficherListeLivre(tabLivre) {
+  let zoneAffiche = document.getElementById("liste");
+  if (tabLivre.length === 0) {
+    alert("la ligne est vide");
+  } else {
+    let tableau =
+      "<table border = '2px'><tr><th>Code ISBN</th><th>Titre</th><th>Auteur</th><th>Nb de pages</th><th>Thème</th></tr>";
+  }
 }
